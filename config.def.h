332640a2dd0587a3ad67eeb9c2de1fca2a2a70af
Oemer Yildiz <oemer.yildiz@tuhh.de>
2020-12-02 13:09:55 +0100

    added dracula theme

--- config.def.h
+++ config.def.h
@@ -4,13 +4,17 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Liberation Mono:size=10"
 };
+static const char col_gray1[]       = "#282a36";
+static const char col_gray3[]       = "#6272a4";
+static const char col_gray4[]       = "#f8f8f2";
+static const char col_purple[]        = "#bd93f9";
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { col_gray3, col_gray1 },
+	[SchemeSel] = { col_gray4, col_purple },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
